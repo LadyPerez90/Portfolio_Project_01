4cad039845a14aedc646b143abc7365af411bbf9
LadyPerez90 <josefa.groce@gmail.com>
2021-04-01 20:29:12 -0500

    Portfolio Update

--- JSON/json.js
+++ JSON/json.js
@@ -1,12 +1,13 @@
-$(document).ready(function() {
-    $.get(url, function(res) {
-        console.log(res.data)
-        var html_str = "";
-        for(var i - 0; i < res.data.length; i++) {
-        html_str += "<div class=''>";
-        //any other html_str goes here
-        html_str += "</div>";
-        }
-        $(".main").html(html_str);
-    }, "json");
+$(document).ready(function(){
+    $('.question').click(function(){
+        $.get("https://opentdb.com/api.php?amount=10&category=15&type=multiple&encode=url3986", function (res){
+            console.log('res.question')
+        })
+    })
+    $('.choice[i]').click(function(){
+        $.get("https://opentdb.com/api.php?amount=10&category=15&type=multiple&encode=url3986", function (res){
+            console.log(res.answer)
+        }, "json")
+    })
 })
+
